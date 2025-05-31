284570d2f55484b7ec28f28a917a15f8d0b0cb9b
Vishal Aakash <111877553+vishalmet@users.noreply.github.com>
2025-05-31 10:20:20 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { Web3Provider } from "./web3-provider";
-import { ReferrerTracker } from "./ReferrerTracker.ts";
+
 const rootElement = document.getElementById("root");
 if (!rootElement) {
   throw new Error("Failed to find root element");
@@ -10,7 +10,7 @@ if (!rootElement) {
 
 createRoot(rootElement).render(
   <Web3Provider>
-    <ReferrerTracker />
+    
     <App />
   </Web3Provider>
 );
